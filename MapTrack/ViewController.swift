853be9881f10bbb6c194c39c3bba923b64b1bb47
Konstantin Kiski <kostyankiski@gmail.com>
2020-07-07 16:38:25 +0400

    add mapview, location

--- MapTrack/ViewController.swift
+++ MapTrack/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var mapView: MGLMapView!
+    
+    // MARK: - Variables
 
+    private let locationManager = CLLocationManager()
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        getLocation()
+        setMapbox()
+
+    }
+    
+    // MARK: - Set content
+    
+    private func setMapbox() {
+        mapView.styleURL = MGLStyle.darkStyleURL
+    }
+    
+    // MARK: - Get location
+    
+    private func getLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .followWithHeading
+            mapView.showsUserHeadingIndicator = true
+        }
+    }
 
 }
 
