3e0a41b63699191b1d6db3d0dac41e0185566c30
Konstantin Kiski <kostyankiski@gmail.com>
2020-07-07 16:07:01 +0400

    Initial Commit

---
+++ MapTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapTrack
+//
+//  Created by Константин Киски on 07.07.2020.
+//  Copyright © 2020 Константин Киски. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
